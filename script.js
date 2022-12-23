3780986f4d81b5ac1c533fb42076e073efe13e5b
OGWhiskers <bayandadlamini02@gmail.com>
2022-12-23 20:19:10 +0200

    Add decideWinner function
    This function decides winner for a single round of rock, paper, scissors
    , so that it can be later used in full rock, paper, scissors game.

--- script.js
+++ script.js
@@ -1,15 +1,54 @@
 function getComputerChoice() {
 
-    let ComChoices = ['Rock', 'Paper', 'Scissors'];
+    let ComChoices = ['rock', 'paper', 'scissors'];
 
     const randomChoice = ComChoices[Math.floor(Math.random() * ComChoices.length)];
 
     return randomChoice;    
+
 };
+
 function getPlayerSelection() {
+
     
     let input = prompt('Rock, Paper or Scissors')
     let playerSelection = input.toLowerCase();
 
     return playerSelection
+}
+
+
+
+
+function decideWinner(playerSelection, computerChoice) {
+
+    computerChoice = getComputerChoice() ;
+    playerSelection = getPlayerSelection();
+    
+    console.log(playerSelection);
+    console.log(computerChoice);
+
+    
+if ((playerSelection === 'rock') && (computerChoice=== 'scissors')) {
+    return 'You win. Rock beats Scissors!'
+    
+} else if ((playerSelection === 'paper') && (computerChoice === 'rock')) {
+    return 'You win. Paper beats Rock!'
+    
+} else if ((playerSelection === 'scissors') && (computerChoice === 'paper')) {
+    return 'You win. Scissor beats Paper!'
+    
+} else if ((playerSelection === 'scissors') && (computerChoice === 'rock')) {
+    return 'You lose. Rock beats Scissors!'
+
+} else if ((playerSelection === 'rock') && (computerChoice === 'paper')) {
+    return 'You lose. Paper beats Rock!'
+
+} else if ((playerSelection === 'paper') && (computerChoice === 'scissor')) {
+    return 'You lose. Scissor beats Paper!'
+
+} else {
+    return 'whats wrong'
+}
+    
 }
