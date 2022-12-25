c88f708ce08c3a27840e01d40224ce27389da337
OGWhiskers <bayandadlamini02@gmail.com>
2022-12-25 18:39:35 +0200

    Add loop that keeps score and plays five rounds
    Loop will be added to game function later.

--- script.js
+++ script.js
@@ -15,105 +15,78 @@ function getPlayerSelection() {
     let playerSelection = input.toLowerCase();
 
     return playerSelection
-}
+};
 
-
-
+    let cpuScore = 0;
+    let playerScore= 0;
 
 function rpsRound(playerSelection, computerChoice) {
 
-    computerChoice = getComputerChoice() ;
-    playerSelection = getPlayerSelection();
+
+    if ((playerSelection === 'rock') && (computerChoice=== 'scissors')) {
+        playerScore++
+        return 'You win. Rock beats Scissors!'
+        
+        
+    } else if ((playerSelection === 'paper') && (computerChoice === 'rock')) {
+        playerScore++
+        return 'You win. Paper beats Rock!'
+        
+
+        
+    } else if ((playerSelection === 'scissors') && (computerChoice === 'paper')) {
+        playerScore++
+        return 'You win. Scissor beats Paper!'
+        
+
+        
+    } else if ((playerSelection === 'scissors') && (computerChoice === 'rock')) {
+        cpuScore++
+        return 'You lose. Rock beats Scissors!'
+        
     
-    // console.log(playerSelection);
-    // console.log(computerChoice);
+    } else if ((playerSelection === 'rock') && (computerChoice === 'paper')) {
+        cpuScore++
+        return 'You lose. Paper beats Rock!'
+        
 
-    function decideWinner(){
+    
+    } else if ((playerSelection === 'paper') && (computerChoice === 'scissor')) {
+        cpuScore++
+        return 'You lose. Scissor beats Paper!'
+        
 
-        let winner = '';
-
-        if((playerSelection === 'rock') && (computerChoice=== 'scissors')){
-            winner = 'Player';
-
-        } else if ((playerSelection === 'paper') && (computerChoice === 'rock')) {
-            winner = 'Player'
-
-        } else if ((playerSelection === 'scissors') && (computerChoice === 'paper')) {
-            winner = 'Player'
-
-        } else if ((playerSelection === 'scissors') && (computerChoice === 'rock')) {
-            winner = 'CPU';
-
-        } else if ((playerSelection === 'rock') && (computerChoice === 'paper')) {
-            winner = 'CPU';
-
-        } else if ((playerSelection === 'paper') && (computerChoice === 'scissor')) {
-            winner = 'CPU';
-
-        } else if (playerSelection === computerChoice) {
-            winner = ''
-        }
-
-            
-        return winner;
+    
+    } else if (playerSelection === computerChoice) {
+        return 'Its a draw!!!'
+        
+    } else {
+        return 'Something went wrong. Did you type a proper input?'
+    }
+    
     };
 
+for (let i = 0; i < 5; i++) {
 
+    let computerChoice = getComputerChoice();
+    let playerSelection = getPlayerSelection();
 
-decideWinner();
+    console.log(rpsRound(playerSelection, computerChoice));
+    console.log(`Computer Score: ${cpuScore} + Player Score: ${playerScore}`);
+  
+}
 
-
-
-    function addScore(){
-
-        let cpuScore = 0;
-        let playerScore= 0;
-        let win = decideWinner();
-        
-        console.log(win);
-
-        if  (win === 'CPU'){     
-
-            console.log(`The Computers Score is: ${cpuScore += 1}`); 
-            return `The Computers Score is: ${cpuScore += 1}`
-
-        } else if (win === 'Player'){
-
-            console.log(`The Computers Score is: ${playerScore += 1}`);
-            return `The Computers Score is: ${playerScore += 1}`
-            
-        } 
-
-    }
-addScore();
-    
-
-
-if ((playerSelection === 'rock') && (computerChoice=== 'scissors')) {
-    return 'You win. Rock beats Scissors!'
-    
-} else if ((playerSelection === 'paper') && (computerChoice === 'rock')) {
-    return 'You win. Paper beats Rock!'
-    
-} else if ((playerSelection === 'scissors') && (computerChoice === 'paper')) {
-    return 'You win. Scissor beats Paper!'
-    
-} else if ((playerSelection === 'scissors') && (computerChoice === 'rock')) {
-    return 'You lose. Rock beats Scissors!'
-
-} else if ((playerSelection === 'rock') && (computerChoice === 'paper')) {
-    return 'You lose. Paper beats Rock!'
-
-} else if ((playerSelection === 'paper') && (computerChoice === 'scissor')) {
-    return 'You lose. Scissor beats Paper!'
-
-} else if (playerSelection === computerChoice) {
-    return 'Its a draw!!!'
-    
+if (playerScore > cpuScore) {
+    console.log('You Won!!!');
+} else if (cpuScore > playerScore) {
+    console.log('You lost');
 } else {
-    return 'Something went wrong. Did you type a proper input?'
+    console.log('Is it a tie?');
 }
 
 
-}
-console.log(rpsRound());
+
+
+
+
+
